ff02ba7c4e8ee9d27c4fb6e2a630a1d59a185f22
Chase Lotito <clotito14@gmail.com>
2025-02-04 19:54:08 -0600

    Have KLOT opened in Simple GUI Window...

--- radarDisplayTest.py
+++ radarDisplayTest.py
@@ -1,9 +1,21 @@
-from PIL import Image
-import matplotlib.pyplot as plt
-import numpy as np
+import urllib.request
+import PySimpleGUI as sg
+from PIL import Image, ImageTk, ImageSequence
 
-klot_loop_gif_path = 'https://radar.weather.gov/ridge/standard/KLOT_loop.gif'
+# retrieve KLOT loop gif from internet
+urllib.request.urlretrieve('https://radar.weather.gov/ridge/standard/KLOT_loop.gif', 'KLOT_loop.gif')
+klot_loop_gif_path = 'KLOT_loop.gif'
+gif_filename = klot_loop_gif_path
 
-img = np.asarray(Image.open(klot_loop_gif_path)) 
+layout = [[sg.Image(key='-IMAGE-')]]
 
-imgplot = plt.imshow(img)
+window = sg.Window('KLOT Standard', layout, element_justification='c', margins=(0,0), element_padding=(0,0), finalize=True)
+
+interframe_duration = Image.open(gif_filename).info['duration']
+
+while True:
+    for frame in ImageSequence.Iterator(Image.open(gif_filename)):
+        event, values = window.read(timeout=interframe_duration)
+        if event == sg.WIN_CLOSED:
+            exit(0)
+        window['-IMAGE-'].update(data=ImageTk.PhotoImage(frame) )
